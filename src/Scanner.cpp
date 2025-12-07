d8ed2b5ac087f790d4f83859c17a515320b7428c
Dmitriy Sukhikh <dimasssuchich@mail.ru>
2025-12-07 20:52:41 +0300

    impl scanner (wout error handling)

---
+++ src/Scanner.cpp
@@ -0,0 +1,182 @@
+#include "Scanner.hpp"
+#include "Definitions.hpp"
+#include <cctype>
+
+// TODO: Обработка ошибок
+// TODO: Заполнение таблицы символов
+
+Scanner::Scanner()
+    : _stream(std::cin)
+{
+}
+
+Scanner::Scanner(std::istream& stream)
+    : _stream(stream) { };
+
+Token Scanner::next()
+{
+    // Зашли ли мы в блок, отделенный кавычками. Если да, то все символы мы
+    // воспринимаем как часть токена WORD_TOKEN
+    static bool is_quoted = false;
+    auto &raw_stream = _stream.get();
+
+    // Скипаем пустые символы (если мы не quoted)
+    if (!is_quoted)
+    {
+        for (auto ch = raw_stream.get(); !raw_stream.eof() && std::isspace(ch);
+            ch = raw_stream.get());
+        if (!raw_stream.eof()) raw_stream.unget();
+    }
+
+    auto ch = raw_stream.get();
+
+
+    if (raw_stream.eof())
+    {
+        return {TokenType::END, ""};
+    }
+
+    if (is_quoted)
+    {
+        if (ch == '"')
+        {
+            is_quoted = false;
+            return { TokenType::QUOTE, "\"" };
+        }
+        raw_stream.unget();
+        return getWord();
+    }
+    else
+    {
+        if (ch == '(')
+        {
+            return { TokenType::LPAR, "(" };
+        }
+
+        else if (ch == ')')
+        {
+            return { TokenType::RPAR, ")" };
+        }
+
+        else if (ch == '"')
+        {
+            is_quoted = true;
+            return { TokenType::QUOTE, "\"" };
+        }
+
+        else if (ch == ';')
+        {
+            return { TokenType::SEMICOL, ";" };
+        }
+
+        else if (ch == '+' || ch == '-')
+        {
+            raw_stream.unget();
+            return getOp();
+        }
+
+        else if (comp.count(ch))
+        {
+            raw_stream.unget();
+            return getCond();
+        }
+
+        else if (std::isdigit(ch))
+        {
+            raw_stream.unget();
+            return getNum();
+        }
+
+        else if (std::isalpha(ch) || ch == '_')
+        {
+            raw_stream.unget();
+            return getName();
+        }
+    }
+
+    return {};
+}
+
+Token Scanner::getCond()
+{
+    auto &raw_stream = _stream.get();
+
+    // Терминалы cond в два символа
+    std::unordered_set<std::string> cond_terms = {"<=", ">=", "!=", "==", "&&", "||"};
+
+    Lexem l;
+    l.push_back(raw_stream.get());
+    l.push_back(raw_stream.get()); // TODO: обработка ошибок
+
+    if (cond_terms.count(l))
+    {
+        return {TokenType::COMP, l};
+    }
+    raw_stream.unget();
+
+    if (l[0] == '>' || l[0] == '<')
+    {
+        return {TokenType::COMP, std::string(1, l[0])};
+    }
+    else if (l[0] == '=')
+    {
+        return {TokenType::ASSIGN_OP, "="};
+    }
+
+    return {}; // TODO: Ошибка
+}
+
+Token Scanner::getNum()
+{
+    auto &r = _stream.get();
+    // Просто извлекаем цифры, пока мы не встретим не-цифру  
+    Lexem l;
+    for (auto ch = r.get(); std::isdigit(ch) && !r.eof(); ch = r.get()) l += ch;
+
+    if (!r.eof()) r.unget();
+
+    if (l.size() > 1 && l[0] == 0) 
+        ; // TODO: Ошибка 
+
+    return {TokenType::NUM, l};
+}
+
+Token Scanner::getName()
+{
+    auto &r = _stream.get();
+    // Просто извлекаем цифры, пока мы не встретим не-цифру  
+    Lexem l;
+    for (auto ch = r.get(); (std::isalnum(ch) || ch == '_') && !r.eof();
+        ch = r.get())
+        l += ch;
+
+    if (!r.eof()) r.unget();
+    
+    if (l == "print") return {TokenType::Print, "print"};
+    if (l == "for") return {TokenType::For, "for"};
+
+    return {TokenType::NAME, l};
+}
+
+Token Scanner::getOp()
+{
+    auto &raw_stream = _stream.get();
+
+    Lexem l;
+    l.push_back(raw_stream.get());
+    l.push_back(raw_stream.get()); // TODO: обработка ошибок
+    
+    if (l == "++" || l == "--") return {TokenType::UNARY_OP, l};
+    return {}; // TODO: обработка ошибок
+}
+
+Token Scanner::getWord()
+{
+    auto &r = _stream.get();
+    // Просто извлекаем цифры, пока мы не встретим не-цифру  
+    Lexem l;
+    for (auto ch = r.get(); ch != '"' && !r.eof(); ch = r.get()) l += ch;
+
+    if (!r.eof()) r.unget();
+    return {TokenType::WORD, l}; 
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "Definitions.hpp"
+#include "Scanner.hpp"
+
+
+std::unordered_map<TokenType, std::string> desc
+{
+    {TokenType::ASSIGN_OP, "ASSIGN_OP"},
+    {TokenType::COMP, "COMP"},
+    {TokenType::END, "END"},
+    {TokenType::For, "For"},
+    {TokenType::LPAR, "LPAR"},
+    {TokenType::NAME, "NAME"},
+    {TokenType::NUM, "NUM"},
+    {TokenType::Print, "Print"},
+    {TokenType::QUOTE, "QUOTE"},
+    {TokenType::RPAR, "RPAR"},
+    {TokenType::SEMICOL, "SEMICOL"},
+    {TokenType::UNARY_OP, "UNARY_OP"},
+    {TokenType::UNDEF, "UNDEF"},
+    {TokenType::WORD, "WORD"}
+};
 
 int main()
 {
-    std::cout << "hello, world!" << std::endl; 
+    std::cout << std::to_string('4') << std::endl;
+    Scanner s;
+
+    for (auto t = s.next(); t.first != TokenType::END; t = s.next())
+    {
+        std::cout << desc[t.first] << " " << t.second << std::endl;
+    }
 }
